f15731f6680c148100686819916ce3b1f3213667
Rahul-404 <rahulshelke981@gmail.com>
2026-04-18 23:55:07 +0530

    corrected bucket name

--- terraform/environment/prod/backend.tf
+++ terraform/environment/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tf-prod-demo-project-bucket-2026"
+    bucket       = "tf-prod-bucket-rahul-2026"
     key          = "prod/terraform.state"
     region       = "ap-south-1"
     encrypt      = true
