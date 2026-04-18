25278b6f16062218bf31bcfea0231ce52e97f65b
Rahul-404 <rahulshelke981@gmail.com>
2026-04-18 17:13:45 +0530

    added provider and variables in staging and prod

---
+++ terraform/environment/prod/variables.tf
@@ -0,0 +1,32 @@
+########################
+#   global variables   #
+########################
+
+variable "project_name" {
+  description = "Name of project"
+  type        = string
+  default     = "demo-project"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "owner" {
+  description = "Owner name"
+  type        = string
+  default     = "rahul-shelke"
+}
+
+variable "additional_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "region" {
+  description = "Name of aws region"
+  type        = string
+  default     = "ap-south-1"
+}
