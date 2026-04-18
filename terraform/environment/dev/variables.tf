b7437e5a6f423045dd7124b6f76030cc1a61f9c5
Rahul-404 <rahulshelke981@gmail.com>
2026-04-18 17:22:06 +0530

    added project_name, owner, additional_tags

--- terraform/environment/dev/variables.tf
+++ terraform/environment/dev/variables.tf
@@ -2,10 +2,27 @@
 #   global variables   #
 ########################
 
+variable "project_name" {
+  description = "Name of project"
+  type        = string
+  default     = "demo-project"
+}
+
 variable "environment" {
   description = "Environment name"
   type        = string
   default     = "dev"
+}
+
+variable "owner" {
+  description = "Owner name"
+  type        = string
+  default     = "rahul-shelke"
+}
+
+variable "additional_tags" {
+  type    = map(string)
+  default = {}
 }
 
 variable "region" {
