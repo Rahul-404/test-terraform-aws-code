1636d43ec5c677bf4327f442ce51a487ce61b97c
Rahul-404 <rahulshelke981@gmail.com>
2026-04-30 19:15:51 +0530

    remove: environment folder

--- terraform/environment/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tf-prod-bucket-rahul-2026"
-    key          = "prod/terraform.state"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
--- terraform/environment/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tf-staging-bucket-rahul-2026"
-    key          = "staging/terraform.state"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
